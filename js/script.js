0e62159b6047e98aa23eb033697eeed39d8892f7
Sebastian Araos <s.araos.olguin@gmail.com>
2022-04-07 16:19:04 -0400

    Ricomida 2punto0
    Correcciones y actualizaciones.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-    
     $('#enviarCorreo').click(function (){
         alert("El correo fue enviado correctamente...");
     });
@@ -12,5 +10,16 @@ $(document).ready(function () {
     $('b').on("dblclick",function (){
         $(this).css('color', 'red');
     });
-
+    $("#panqueques").click(function(){
+        $('#receta-texto').toggle(1000,function() {
+        });
+    });
+    $("#tiramisu").click(function(){
+        $('#receta-texto2').toggle(1000,function() {
+        });
+    });
+    $("#plateada").click(function(){
+        $('#receta-texto3').toggle(1000,function() {
+        });
+    });
 });
